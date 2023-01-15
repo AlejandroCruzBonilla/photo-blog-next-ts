cc105f59dc6bcf57bd7ab021362bd1a067361304
Alejandro Cruz Bonilla <alex.139139.cb@gmail.com>
2023-01-15 14:27:02 -0600

    optimizaciones

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,34 +5,6 @@ export default function Document() {
 		<Html lang="es">
 			<Head>
 				<link rel="icon" href="/home.ico" />
-				{/* <link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
-				/> */}
-				{/* <link
-					href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined"
-					rel="stylesheet"
-				/> */}
-
-				<link
-					rel="preload"
-					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-					as="style"
-				/>
-
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-				/>
-				<link
-					rel="preload"
-					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
-					as="style"
-				/>
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
-				/>
 			</Head>
 			<body>
 				<Main />
